4b8f8624dbe07bf85d9b3cc2feb91d82c722e738
Soumik Paul <soumik.paul@contentstack.com>
2023-03-23 19:19:42 +0530

    Added producer and consumer

---
+++ producer/producer.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+	conn, _ := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", "kafka-test", 0)
+	conn.SetWriteDeadline(time.Now().Add(time.Second * 10))
+	conn.WriteMessages(kafka.Message{
+		Value: []byte("This is [golang<new>] message"),
+	}, kafka.Message{
+		Value: []byte("This is [golang1<new>] message"),
+	})
+	conn.Close()
+}
